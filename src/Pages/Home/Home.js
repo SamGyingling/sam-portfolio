a745e4d5bb00febe7f4035abf0f69ef818d93abe
samdeng <s747855301@gmail.com>
2022-04-03 15:39:12 +1000

    -initial setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../Components/Header/Header';
+
+function Home() {
+  return (
+    <>
+      <div>Home</div>
+      <Header />
+    </>
+  );
+}
+
+export default Home;
